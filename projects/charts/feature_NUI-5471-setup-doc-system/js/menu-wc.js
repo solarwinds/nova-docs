31c5199f560c54bd080956f614a1afdbd78e1f0c
swi-global-circleci <circleci@circleci>
2021-01-28 19:21:55 +0000

    add docs

--- projects/charts/feature_NUI-5471-setup-doc-system/js/menu-wc.js
+++ projects/charts/feature_NUI-5471-setup-doc-system/js/menu-wc.js
@@ -475,19 +475,6 @@ customElements.define('compodoc-menu', class extends HTMLElement {
                             </li>
                         </ul>
                     </li>
-                        <li class="chapter">
-                            <div class="simple menu-toggler" data-toggle="collapse" ${ isNormalMode ? 'data-target="#injectables-links"' :
-                                'data-target="#xs-injectables-links"' }>
-                                <span class="icon ion-md-arrow-round-down"></span>
-                                <span>Injectables</span>
-                                <span class="icon ion-ios-arrow-down"></span>
-                            </div>
-                            <ul class="links collapse" ${ isNormalMode ? 'id="injectables-links"' : 'id="xs-injectables-links"' }>
-                                <li class="link">
-                                    <a href="injectables/GaugeService.html" data-type="entity-link">GaugeService</a>
-                                </li>
-                            </ul>
-                        </li>
                     <li class="chapter">
                         <div class="simple menu-toggler" data-toggle="collapse" ${ isNormalMode ? 'data-target="#interfaces-links"' :
                             'data-target="#xs-interfaces-links"' }>
