a838ceafcd054ab16aa9aeab7c28c0a0692a785f
dmytrognatyshyn <Dmytro.Gnatyshyn@solarwinds.com>
2021-07-12 13:11:14 +0300

    Upd header component

--- client/nova-docs-client/src/app/header/header.component.ts
+++ client/nova-docs-client/src/app/header/header.component.ts
@@ -1,14 +1,16 @@
 import {
   AfterViewInit,
+  ChangeDetectionStrategy,
   Component,
   EventEmitter,
+  Inject,
   OnInit,
   Output,
   ViewChild
 } from '@angular/core';
 import {AppService} from "../../services/app.service";
-import {SelectV2Component} from "@nova-ui/bits";
-import {Observable} from "rxjs";
+import {IToastService, SelectV2Component, ToastService} from "@nova-ui/bits";
+import {environment} from "../../environments/environment";
 
 export interface IProject {
   branches: Array<IBranch>;
@@ -24,23 +26,23 @@ export interface IBranch {
   selector: 'nui-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.less'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HeaderComponent implements OnInit, AfterViewInit {
   public projects: IProject[] = [];
   public activeProject: IProject = {} as IProject;
-  public selectedBranch: string = '';
+  public selectedBranch: string = "";
+  private host = environment.production ? "" : environment.apiUrl;
 
-  @ViewChild("select") private select?: SelectV2Component;
+  @ViewChild("select") public select: SelectV2Component;
 
   @Output() selectedUrl = new EventEmitter<string>();
 
-  constructor(private appService: AppService) { }
+  constructor(private appService: AppService,
+              @Inject(ToastService) private toastService: IToastService) { };
 
   public ngOnInit(): void {
-    this.getProjects().subscribe(projects => {
-      this.projects = projects;
-      this.setActiveProject(projects);
-    })
+    this.getProjects();
   }
 
   public ngAfterViewInit(): void {
@@ -54,23 +56,27 @@ export class HeaderComponent implements OnInit, AfterViewInit {
   public openDocs(project: IProject, branchName: string = this.selectedBranch): void {
     this.activeProject = project;
     this.selectedBranch = branchName;
-    const url = `${project.name}/${branchName}`
+    const url = `${this.host}/${project.name}/${branchName}`
     this.selectedUrl.emit(url);
   }
 
-  private getProjects(): Observable<Array<IProject>> {
-    return this.appService.getProjects();
+  private getProjects(): void {
+    this.appService.getProjects().subscribe(projects => {
+        this.projects = projects;
+        this.setActiveProject(projects);
+      },
+      error => this.toastService.error({ title: error.message }));
   }
 
   private setActiveProject(projects: Array<IProject>): void {
-    if (!location.hash || location.hash === "#/") {
+    if (!location.hash || location.hash === "#/" || location.hash === "#") {
       this.activeProject = projects[0];
       this.selectedBranch = this.activeProject.branches[0].name;
       this.openDocs(this.activeProject, this.selectedBranch);
     } else {
-      const hashArray = location.hash.split('/');
-      this.activeProject = projects.find((project: IProject) => project.name === hashArray[1]) || projects[0];
-      this.selectedBranch = hashArray[2];
+      const arrayFromHash = location.hash.split('/');
+      this.activeProject = projects.find((project: IProject) => project.name === arrayFromHash[1]) || projects[0];
+      this.selectedBranch = this.activeProject.branches.find((branch: IBranch) => branch.name === arrayFromHash[2])?.name || this.activeProject.branches[0].name;
       const convertedUrl = `${window.location.origin}/${location.hash.replace("#/", "")}`;
       this.selectedUrl.emit(convertedUrl);
     }
